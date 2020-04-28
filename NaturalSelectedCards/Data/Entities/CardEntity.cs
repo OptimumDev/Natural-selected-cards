21f7f9339df40fd234530c1dbaf225c84517d793
TVIGes <khdv66@gmail.com>
2020-04-28 19:05:45 +0500

    Create database entities

---
+++ NaturalSelectedCards/Data/Entities/CardEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NaturalSelectedCards.Data.Entities
+{
+    public class CardEntity
+    {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Local
+        public Guid Id { get; private set; }
+        public Guid DeckId { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public int Repetitions { get; set; }
+        public int CorrectAnswers { get; set; }
+        public DateTime LastRepeat { get; set; }
+    }
+}
---
+++ NaturalSelectedCards/Data/Entities/DeckEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NaturalSelectedCards.Data.Entities
+{
+    public class DeckEntity
+    {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Local
+        public Guid Id { get; private set; }
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+    }
+}
