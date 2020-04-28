81ff6253a2b68835eca4cf76a1af4e31b3e77e4f
TVIGes <khdv66@gmail.com>
2020-04-28 19:13:52 +0500

    Define repository interfaces for decks and cards

---
+++ NaturalSelectedCards/Data/Repositories/ICardRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NaturalSelectedCards.Data.Entities;
+
+namespace NaturalSelectedCards.Data.Repositories
+{
+    public interface ICardRepository
+    {
+        Task<List<CardEntity>> GetCardsByDeckAsync(Guid deckId);
+        Task<CardEntity> FindByIdAsync(Guid cardId);
+        Task<CardEntity> InsertAsync(CardEntity card);
+        Task UpdateAsync(CardEntity card);
+        Task DeleteAsync(Guid cardId);
+        Task DeleteByDeckAsync(Guid deckId);
+    }
+}
