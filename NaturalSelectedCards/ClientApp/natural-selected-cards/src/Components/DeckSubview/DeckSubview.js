4a10cc016df5d5b3bc4dfb1dc055c2cdd561805e
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-04-28 19:53:41 +0500

    edit page card choosing and flipping

--- NaturalSelectedCards/ClientApp/natural-selected-cards/src/Components/DeckSubview/DeckSubview.js
+++ NaturalSelectedCards/ClientApp/natural-selected-cards/src/Components/DeckSubview/DeckSubview.js
@@ -1,21 +1,28 @@
 import React from "react";
 import './DeckSubview.css';
 
-export default function DeckSubview({deck}) {
-    return (
-        <div className='deck-subview'>
-            {deck.cards.map(renderCard)}
+export default class DeckSubview extends React.Component {
+    render() {
+        return (
+            <div className='deck-subview'>
+                {this.props.deck.cards.map(this.renderCard)}
+            </div>
+        );
+    }
+
+    renderCard = (card, i) => (
+        <div key={card.id}
+             className='card-container'
+             onClick={() => this.props.onCardChoice(i)}
+        >
+            <div className={`card ${this.props.chosenIndex === i ? 'chosen' : ''}`}>
+                <div className='card-front main-color shadow'>
+                    {card.front}
+                </div>
+                <div className='card-back main-color-light shadow'>
+                    {card.back}
+                </div>
+            </div>
         </div>
     );
 }
-
-const renderCard = card => (
-    <div key={card.id} className='card'>
-        <div className='card-front main-color shadow'>
-            {card.front}
-        </div>
-        <div className='card-back main-color-light shadow'>
-            {card.back}
-        </div>
-    </div>
-);
--- NaturalSelectedCards/ClientApp/natural-selected-cards/src/Components/Pages/EditPage/EditPage.js
+++ NaturalSelectedCards/ClientApp/natural-selected-cards/src/Components/Pages/EditPage/EditPage.js
@@ -5,31 +5,57 @@ import CardCarousel from "../../CardCarousel/CardsCorusel";
 import FlipCard from "../../FlipCard/FlipCard";
 import './EditPage.css'
 
-export default function EditPage({deckId}) {
-    const deck =
-        standardDecks.find(deck => deck.id === deckId) ||
-        myDecks.find(deck => deck.id === deckId);
+export default class EditPage extends React.Component {
 
-    return (
-        <div className='page'>
-            <div className='page-name'>
-                Edit page
+    constructor(props, context) {
+        super(props, context);
+
+        this.deck =
+            standardDecks.find(deck => deck.id === this.props.deckId) ||
+            myDecks.find(deck => deck.id === this.props.deckId);
+
+        this.state = {
+            cardIndex: 0,
+            flipped: this.deck.cards.map(_ => false)
+        };
+    }
+
+    render() {
+        return (
+            <div className='page'>
+                <div className='page-name'>
+                    Edit page
+                </div>
+                <CardCarousel
+                    cardIndex={this.state.cardIndex}
+                    buttons={[
+                        <button key={1} className='main-color shadow' onClick={this.flip}>Перевернуть</button>
+                    ]}
+                >
+                    {this.deck.cards.map(this.renderCard)}
+                </CardCarousel>
+                <DeckSubview deck={this.deck} chosenIndex={this.state.cardIndex} onCardChoice={this.setCardIndex}/>
             </div>
-            <CardCarousel cardIndex={0}>
-                {deck.cards.map(renderCard)}
-            </CardCarousel>
-            <DeckSubview deck={deck}/>
-        </div>
-    );
-}
+        );
+    }
 
-const renderCard = card => (
-    <FlipCard key={card.id}>
-        <div className='card-side'>
-            <input type='text' defaultValue={card.front} className='main-color'/>
-        </div>
-        <div className='card-side'>
-            <input type='text' defaultValue={card.back} className='main-color-light'/>
-        </div>
-    </FlipCard>
-);
+    renderCard = (card, i) => (
+        <FlipCard key={card.id} flipped={this.state.flipped[i]}>
+            <div className='card-side'>
+                <input type='text' defaultValue={card.front} className='main-color'/>
+            </div>
+            <div className='card-side'>
+                <input type='text' defaultValue={card.back} className='main-color-light'/>
+            </div>
+        </FlipCard>
+    );
+
+    setCardIndex = i => this.setState({
+        cardIndex: i,
+        flipped: this.deck.cards.map(_ => false)
+    });
+
+    flip = () => this.setState({
+        flipped: this.state.flipped.map((f, i) => i === this.state.cardIndex ? !f : f)
+    });
+}
