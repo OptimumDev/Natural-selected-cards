bb22aeb18842eb16b874c0fa411f2e5388835c72
Petrov Sergiy Sergeevich <s.s.petrov@tinkoff.ru>
2020-05-02 12:39:31 +0500

    add interface for deck manager

---
+++ NaturalSelectedCards/Logic/Managers/IDeckManager.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NaturalSelectedCards.Data.Entities;
+
+namespace NaturalSelectedCards.Logic.Managers
+{
+    public interface IDeckManager
+    {
+        Task<List<DeckEntity>> GetDecksFor(Guid userId);
+        Task<List<DeckEntity>> GetStandartDecks();
+        Task<Boolean> AddStandartDeck(Guid userId, Guid deckId);
+        // придумать что возвращать bool, или колоду
+        Task<List<CardEntity>> GetAllCardsFromDeck(Guid deckId);
+        Task<DeckEntity> UpdateDeck(DeckEntity deck);
+        Task<DeckEntity> AddDeck(Guid userId, DeckEntity deck);
+        Task<Boolean> DeleteDeck(Guid userId, Guid deckId);
+        Task<List<CardEntity>> GetCardsForGame(Guid userId, Guid deckId);
+        Task<Boolean> UpdateCardKnowledge(Guid userId, Guid cardId, Boolean isCorrect);
+    }
+}
