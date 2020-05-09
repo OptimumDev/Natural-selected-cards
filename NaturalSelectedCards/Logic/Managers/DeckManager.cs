2bff8772ab1cbe98aaaf849372fa2c6569c200e4
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-05-09 12:19:07 +0500

    code cleanup

--- NaturalSelectedCards/Logic/Managers/DeckManager.cs
+++ NaturalSelectedCards/Logic/Managers/DeckManager.cs
@@ -8,77 +8,81 @@ using NaturalSelectedCards.Logic.Mappers;
 
 namespace NaturalSelectedCards.Logic.Managers
 {
-    public class DeckManager: IDeckManager
+    public class DeckManager : IDeckManager
     {
-        private IDeckRepository deckRepository;
-        private ICardRepository cardRepository;
-        private DeckMapper deckMapper;
-        private CardMapper cardMapper;
+        private readonly IDeckRepository deckRepository;
+        private readonly ICardRepository cardRepository;
+        private readonly DeckMapper deckMapper;
+        private readonly CardMapper cardMapper;
 
-        public DeckManager(IDeckRepository deckRepository, ICardRepository cardRepository, DeckMapper deckMapper, CardMapper cardMapper) {
+        public DeckManager(IDeckRepository deckRepository, ICardRepository cardRepository,
+            DeckMapper deckMapper, CardMapper cardMapper)
+        {
             this.deckRepository = deckRepository;
             this.cardRepository = cardRepository;
             this.deckMapper = deckMapper;
             this.cardMapper = cardMapper;
         }
 
-        public async Task<List<DeckModel>> GetDecksAsync(Guid userId) {
+        public async Task<List<DeckModel>> GetDecksAsync(Guid userId)
+        {
             var decks = await deckRepository.GetUserDecksAsync(userId);
             return decks.ConvertAll(deck => deckMapper.Map(deck));
         }
 
-        public async Task<List<DeckModel>> GetStandartDecksAsync() {
+        public async Task<List<DeckModel>> GetStandardDecksAsync()
+        {
             var decks = await deckRepository.GetStandardDecksAsync();
             return decks.ConvertAll(deck => deckMapper.Map(deck));
         }
 
-        public async Task<bool> CopyDeckAsync(Guid userId, Guid deckId) {
+        public async Task<bool> CopyDeckAsync(Guid userId, Guid deckId)
+        {
             var deck = await deckRepository.FindByIdAsync(deckId);
-            if (deck != null)
-            {
-                var result = await deckRepository.InsertAsync(deck);
-                if (result != null)
-                    return true;
-            }
-            return false;
+            if (deck == null)
+                return false;
+            var result = await deckRepository.InsertAsync(deck);
+            return result != null;
         }
 
-        public async Task<List<CardModel>> GetAllCardsFromDeckAsync(Guid deckId) {
+        public async Task<List<CardModel>> GetAllCardsFromDeckAsync(Guid deckId)
+        {
             var cards = await cardRepository.GetCardsByDeckAsync(deckId);
             return cards.ConvertAll(card => cardMapper.Map(card));
         }
 
-        public async Task<Guid> AddDeckAsync(Guid userId) {
+        public async Task<Guid?> AddDeckAsync(Guid userId)
+        {
             var emptyDeck = new DeckEntity(userId, "");
             var entity = await deckRepository.InsertAsync(emptyDeck);
-            return entity.Id;
+            return entity?.Id;
         }
 
-        public async Task<bool> UpdateDeckTitleAsync(Guid deckId, String title) {
+        public async Task<bool> UpdateDeckTitleAsync(Guid deckId, string title)
+        {
             var deck = await deckRepository.FindByIdAsync(deckId);
-            if (deck == null) {
+            if (deck == null)
                 return false;
-            }
             var updatedDeck = new DeckEntity(deck.Id, deck.UserId, title);
             var result = await deckRepository.UpdateAsync(updatedDeck);
-            if (result == null) {
-                return false;
-            }
-            return true;
+            return result != null;
         }
 
-        public async Task<bool> DeleteDeckAsync(Guid deckId) {
+        public async Task<bool> DeleteDeckAsync(Guid deckId)
+        {
             await deckRepository.DeleteAsync(deckId);
             return true;
         }
 
-        public async Task<List<CardModel>> GetCardsForGameAsync(Guid deckId) {
+        public async Task<List<CardModel>> GetCardsForGameAsync(Guid deckId)
+        {
             // TODO: шафлить и фильтровать карточки
             var cards = await cardRepository.GetCardsByDeckAsync(deckId);
             return cards.ConvertAll(card => cardMapper.Map(card));
         }
 
-        public async Task<bool> UpdateCardKnowledgeAsync(Guid cardId, Boolean isCorrect) {
+        public async Task<bool> UpdateCardKnowledgeAsync(Guid cardId, bool isCorrect)
+        {
             var card = await cardRepository.FindByIdAsync(cardId);
             if (card == null)
                 return false;
@@ -92,21 +96,18 @@ namespace NaturalSelectedCards.Logic.Managers
                 DateTime.Now
             );
             var updatedEntity = await cardRepository.UpdateAsync(updatedCard);
-            if (updatedEntity == null)
-                return false;
-            
-            return true;
+            return updatedEntity != null;
         }
 
-        public async Task<Guid?> AddCardAsync(Guid deckId) {
+        public async Task<Guid?> AddCardAsync(Guid deckId)
+        {
             var card = new CardEntity(deckId, "", "");
             var inserted = await cardRepository.InsertAsync(card);
-            if (inserted == null)
-                return null;
-            return inserted.Id;
+            return inserted?.Id;
         }
 
-        public async Task<bool> UpdateCardAsync(CardModel cardModel) {
+        public async Task<bool> UpdateCardAsync(CardModel cardModel)
+        {
             var entity = await cardRepository.FindByIdAsync(cardModel.Id);
             if (entity == null)
                 return false;
@@ -120,13 +121,11 @@ namespace NaturalSelectedCards.Logic.Managers
                 entity.LastRepeat
             );
             var result = await cardRepository.UpdateAsync(updatedEntity);
-            if (result == null)
-                return false;
-            
-            return true;
+            return result != null;
         }
 
-        public async Task<bool> DeleteCardAsync(Guid cardId) {
+        public async Task<bool> DeleteCardAsync(Guid cardId)
+        {
             await cardRepository.DeleteAsync(cardId);
             return true;
         }
