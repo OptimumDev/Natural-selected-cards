acb669985b51f7b3b9b29f7a2a9c2d864c746d4d
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-05-09 16:33:00 +0500

    fix link opening and text

--- natural-selected-cards/src/Components/Pages/MainPage/MainPage.js
+++ natural-selected-cards/src/Components/Pages/MainPage/MainPage.js
@@ -13,12 +13,14 @@ export default function MainPage({onLogin, isDarkTheme}) {
                 Добро пожаловать!<br/>
                 Данный сайт предоставляет возможность<br/>
                 потренироваться в запоминании информации<br/>
-                по <a href='https://en.wikipedia.org/wiki/Flashcard'>методу флэш-карточек</a>.
+                <a href='https://en.wikipedia.org/wiki/Flashcard' target="_blank" rel="noopener noreferrer">
+                    по методу флэш-карточек
+                </a>
             </p>
             <p>
                 Для того, чтобы начать,<br/>
                 <button onClick={() => onLogin(user)} className='main-color shadow enter-button'>Войдите</button><br/>
-                и добавьте в коллекцию первую колоду.<br/>
+                и добавьте в коллекцию первую колоду<br/>
             </p>
             <GoogleLogin
                 onSuccess={x => console.log(x) || onLogin(user)}
