9e05975cd6e7832aa1c9b7cbc72086aff093bb45
TVIGes <khdv66@gmail.com>
2020-05-14 20:16:01 +0500

    Add user repository

---
+++ NaturalSelectedCards/Data/Repositories/MongoUserRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using NaturalSelectedCards.Data.Entities;
+
+namespace NaturalSelectedCards.Data.Repositories
+{
+    public class MongoUserRepository : IUserRepository
+    {
+        public const string CollectionName = "users";
+        
+        private readonly IMongoCollection<UserEntity> _userCollection;
+
+        public MongoUserRepository(IMongoDatabase database)
+        {
+            _userCollection = database.GetCollection<UserEntity>(CollectionName);
+            _userCollection.Indexes.CreateOne(new CreateIndexModel<UserEntity>(
+                Builders<UserEntity>.IndexKeys.Hashed(u => u.GoogleId)
+            ));
+        }
+        
+        public Task<UserEntity> FindByIdAsync(Guid userId)
+        {
+            return _userCollection.Find(u => u.Id == userId).FirstOrDefaultAsync();
+        }
+
+        public Task<UserEntity> FindByGoogleIdAsync(string googleId)
+        {
+            return _userCollection.Find(u => u.GoogleId == googleId).FirstOrDefaultAsync();
+        }
+
+        public async Task<UserEntity> InsertAsync(UserEntity user)
+        {
+            await _userCollection.InsertOneAsync(user);
+            return user;
+        }
+
+        public async Task<UserEntity> UpdateAsync(UserEntity user)
+        {
+            await _userCollection.ReplaceOneAsync(u => u.Id == user.Id, user);
+            return user;
+        }
+
+        public Task DeleteAsync(Guid userId)
+        {
+            return _userCollection.DeleteOneAsync(u => u.Id == userId);
+        }
+    }
+}
