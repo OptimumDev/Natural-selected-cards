b7e9c37ea7f52b5c66ed4537ccb0aa9ff3062f33
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-05-22 21:51:26 +0500

    styles fixes

--- natural-selected-cards/src/Components/Loading/Loading.js
+++ natural-selected-cards/src/Components/Loading/Loading.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./Loading.css"
+
 import ReactLoading from 'react-loading';
 
 export default function Loading() {
     return (
-        <div className='page'>
+        <div className='page loading-page'>
             <ReactLoading type='bubbles' color='#66E2FF' width='30vw' height='30vw'/>
         </div>
     );
