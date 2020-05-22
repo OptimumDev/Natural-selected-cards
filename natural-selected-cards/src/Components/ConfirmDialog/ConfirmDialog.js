1dac32b1c7c403d54969d8bd375319f6b5f34c31
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2020-05-22 22:19:56 +0500

    create confirm dialog

---
+++ natural-selected-cards/src/Components/ConfirmDialog/ConfirmDialog.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './ConfirmDialog.css';
+
+export default function ConfirmDialog({onAccept, onCancel, children}) {
+    return (
+        <div className='dialog-container'>
+            <div className='confirm-dialog shadow'>
+                <div className='confirm-message'>
+                    {children}
+                </div>
+                <footer className='confirm-dialog-footer'>
+                    <button className='shadow main-color' onClick={onCancel}>
+                        Отмена
+                    </button>
+                    <button className='shadow red' onClick={onAccept}>
+                        Удалить
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
